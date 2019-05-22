4e3845860a9e28d72093841d2cc305e06d1ff493
Miko Sramek <mikoggsramek@hotmail.com>
2019-05-22 12:29:43 -0400

    Initial Commit

---
+++ Executor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank {
+	class Executor {
+		static void Main(string[] args) {
+			ProblemSolving.Staircase.Run();	
+		}
+	}
+}
